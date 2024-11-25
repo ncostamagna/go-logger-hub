4033743546d6b7d56fab60958be2e1087e0d899c
Nahue <nlcostamagna@gmail.com>
2024-11-25 19:50:31 -0300

    adding example

--- example/main.go
+++ example/main.go
@@ -8,10 +8,28 @@ import (
 )
 
 func main() {
-	logger := loghub.New(loghub.NewNativeLogger(
-		log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile),
-		15,
-	))
+
+	// Custom Logger
+	customLogger := loghub.CustomLogger{
+		Debug: func(msg string) {
+			log.Println("DEBUG custom: ", msg)
+		},
+		Info: func(msg string) {
+			log.Println("INFO custom: ", msg)
+		},
+		Warn: func(err error) {
+			log.Println("WARN custom: ", err)
+		},
+		Error: func(err error) {
+			log.Println("ERROR custom: ", err)
+		},
+	}
+
+
+	logger := loghub.New(
+		loghub.NewNativeLogger(log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile),15),	
+		loghub.NewCustomLogger(customLogger, 15),
+	)
 
 	logger.Debug("my message Debug")
 	logger.Info("my message Info")
